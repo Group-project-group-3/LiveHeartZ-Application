1df5e7b2047d0688a50cb355a1a41c624e10e3e5
Nufla Nasrudeen <87080421+Nufla-Nasrudeen@users.noreply.github.com>
2023-12-20 00:22:08 +0530

    Update Footer.js

--- frontend/src/Components/Footer.js
+++ frontend/src/Components/Footer.js
@@ -1,1 +1,7 @@
+import React from 'react';
+import "./Footer.css"; 
+import { Container, Row, Col } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram, faYoutube, faFacebook, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import logo from "./logo1@2x.png";
 
