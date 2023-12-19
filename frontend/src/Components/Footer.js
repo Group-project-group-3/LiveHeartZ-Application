4bba1ba868c2cc0e4a8088dab53a0b827fe42c19
Nufla Nasrudeen <87080421+Nufla-Nasrudeen@users.noreply.github.com>
2023-12-20 00:38:19 +0530

    Update Footer.js-copyrights

--- frontend/src/Components/Footer.js
+++ frontend/src/Components/Footer.js
@@ -21,8 +21,32 @@ function Footer() {
               </ul>
             </section>
           </Col>
+          <Col xs={12} lg={4} className="text-center fw-bold">
+            <section>
+              <p className='h2' style={{ color:'#BC005A' }}>Contact Us</p>
+              <ul className="contact-info">
+                <li><p>+94123595684</p></li>
+                <li><p>+94123595684</p></li>
+                <li><p>live.heartz@yahoo.com</p></li>
+                <li><p>live.heartz@yahoo.com</p></li>
+              </ul>
+            </section>
+          </Col>
+          <Col xs={12} lg={4} className="text-center">
+            <section className="img-container">
+              <a href="/link-to-your-logo-page">
+                <img src={logo} className="img-fluid" alt="LiveHeartZ Logo" />
+              </a>
+              <p className='h4'>LiveHeartZ</p>
+            </section>
+          </Col>
         </Row>
       </Container>
+      <hr></hr>
+      <Row className='text-center fw-bold mb-0'>
+        <p className=' mt-0 mb-0'>Copyright @ 2023 All Rights Reserved By <span> Group 03</span></p>
+        <p>Designed & Developed by <span>Group 03</span></p>
+      </Row>
     </div>
   );
 }
