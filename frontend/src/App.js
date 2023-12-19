e775b1eeb3e8bf10ebda9f4a64be0c35bf8c0414
Nufla Nasrudeen <87080421+Nufla-Nasrudeen@users.noreply.github.com>
2023-12-19 23:09:10 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Registration from './Pages/Registration';
+import OtpRegistration from './Pages/OtpRegistration';
+import OtpRequest from './Pages/OtpRequest';
+function App() {
+  return (
+    <div className="App">
+     
+    </div>
+  );
+}
+
+export default App;
