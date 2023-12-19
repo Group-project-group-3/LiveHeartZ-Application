8c2376485aafcbde6bdec36a7f9b1567fc3a072d
Nufla Nasrudeen <87080421+Nufla-Nasrudeen@users.noreply.github.com>
2023-12-20 00:29:29 +0530

    Update Footer.js

--- frontend/src/Components/Footer.js
+++ frontend/src/Components/Footer.js
@@ -5,3 +5,26 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagram, faYoutube, faFacebook, faTwitter } from '@fortawesome/free-brands-svg-icons';
 import logo from "./logo1@2x.png";
 
+function Footer() {
+  return (
+    <div className='footer'>
+      <Container>
+        <Row>
+          <Col xs={12} lg={4} className="text-center">
+            <section>
+              <p className='h2' style={{ color:'#BC005A' }}>Follow Us</p>
+              <ul className="social-icons justify-content-center">
+                <li><a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faInstagram} className="icon" /></a></li>
+                <li><a href="https://www.youtube.com/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faYoutube} className="icon" /></a></li>
+                <li><a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faFacebook} className="icon" /></a></li>
+                <li><a href="https://twitter.com/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faTwitter} className="icon" /></a></li>
+              </ul>
+            </section>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default Footer;
