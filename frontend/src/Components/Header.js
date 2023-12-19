609b8a8839a9817abedc081928e4e0406d290e83
Nufla Nasrudeen <87080421+Nufla-Nasrudeen@users.noreply.github.com>
2023-12-19 23:41:34 +0530

    import necessary packages

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -1,1 +1,6 @@
-import react from 'react';
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPhone, faHome } from '@fortawesome/free-solid-svg-icons';
+import { Image, Container, Row, Col, Button, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import "./Header.css";
+import logo from "./logo1@2x.png";
