bc5afbcd67ea4f7aaf9be8b93e5e096280304ad7
Tekshan Madhawa <tekshanmadhawa324@gmail.com>
2023-12-20 23:27:41 +0530

    Update Login.js

--- frontend/src/Pages/Login.js
+++ frontend/src/Pages/Login.js
@@ -3,7 +3,11 @@ import Header from '../Components/Header';
 import Footer from '../Components/Footer';
 
 function Login(){
-
+    return(
+        <>
+            <h1>Login</h1>
+        </>
+    )
 }
 
 export default Login;
