6bfa5a7e31005c1959f5725b2f66968385d0afbb
Nufla Nasrudeen <87080421+Nufla-Nasrudeen@users.noreply.github.com>
2023-12-20 15:43:53 +0530

    import Header and Footer

--- frontend/src/Pages/Registration.js
+++ frontend/src/Pages/Registration.js
@@ -1,1 +1,13 @@
-import react from 'react';
+import Header from "../Components/Header";
+import Footer from "../Components/Footer";
+import "./Registration.css";
+import { Container, Row, Col, Button, Form } from 'react-bootstrap';
+
+export default function Registration() {
+  return (
+    <div>
+      <Header></Header>
+      <Footer></Footer>
+    </div>
+  );
+}
