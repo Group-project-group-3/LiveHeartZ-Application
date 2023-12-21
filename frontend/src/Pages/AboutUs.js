da92dc6419c184b835142743024f0c3b3540ba64
Tekshan Madhawa <tekshanmadhawa324@gmail.com>
2023-12-21 12:11:48 +0530

    Created AboutUs page

---
+++ frontend/src/Pages/AboutUs.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../Components/Header';
+import Footer from '../Components/Footer';
+
+function AboutUs(){
+    return(
+        <>
+            <Header/>
+        </>
+    )
+}
+
+export default AboutUs;
