2404913958409910045b459b27adebb08526358e
FathimaAzma <140784165+FathimaAzma@users.noreply.github.com>
2023-12-31 09:48:27 +0530

    Committed  header and footer

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,1 +1,11 @@
+import Header from "../Components/Header";
+import Footer from "../Components/Footer";
 
+export default function Home(){
+    return(
+        <div>
+            <Header></Header>
+            <Footer></Footer>
+        </div>
+    );
+}
