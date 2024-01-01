c838794a2b82176dd03cfbaa669a5097038a0ee0
FathimaAzma <140784165+FathimaAzma@users.noreply.github.com>
2024-01-01 19:28:10 +0530

    Updated  paragraph

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -70,54 +70,6 @@ export default function Home(){
 
                         </Row>
                 </Container>
-
-                <div className="banner">
-                        <Container className="mt-5 mb-3 ">
-                            <p className="banner-title">Give Blood <br/>Save A LIFE</p>
-                            <br/>
-                            <p className="banner-para ps-6 pe-6">If a consumer of a REST service needs to hard-code all the resource URLs,<br/>then it is tightly then it is loosely coupled. There is no tight. There is no tight<br/> dependency on the URI structure, <br/>as it is specified and used from the response.</p>
-                            <br/>
-                            <div className="text-center">
-                                <Button  className="joinus-button mt-6 ps-4 pe-4 mt-6 mb-2" style={{ backgroundColor: '#000000', color: '#BC005A', border: '2px solid  #BC005A' }}>
-                                    <b className="login2">Join Us</b>
-                                </Button>
-                            </div>
-                        </Container>
-                </div>
-
-                <div>
-                        <Container className="request_find_Sec mt-5 mb-3">
-                            <Row>
-                                <Col className="ms-1 mt-5 ps-3 pt-4 pb-4 pe-4">
-                                    <div className="req_blood">
-                                        <div className="img_req_blood">
-                                            <Image src="blooddrop-1@2x.png" style={{height:"280px", width:"450px"}}></Image>
-                                        </div>
-
-                                        <h4 className="mt-2">REQUEST BLOOD</h4>
-                                        <p>Some quick example text to build on the card title and make up the bulk of the card's content.<br/>Go somewhere</p>
-                                        <Button className="bestDonor-button mt-6 ps-4 pe-4  mb-1" style={{ backgroundColor: '#BC005A', color: '#FFFFFF', border: '2px solid #BC005A' , textAlign: 'center'}}>
-                                        <b className="login2">Request Blood</b>
-                                        </Button>
-                                    </div>
-                                </Col>
-
-                                <Col className="ms-1 ps-3 mt-5 pt-4 pb-4 pe-4">
-                                    <div className="find_blood_donor">
-                                        <div className="img_donor_blood">
-                                            <Image src="blooddonor-1@2x.png" style={{height:"280px", width:"450px"}}></Image>
-                                        </div>
-
-                                        <h4 className="mt-2">FIND BLOOD DONOR</h4>
-                                        <p>Some quick example text to build on the card title and make up the bulk of the card's content.<br/>Go somewhere</p>
-                                        <Button className="bestDonor-button mt-6 ps-4 pe-4  mb-1" style={{ backgroundColor: '#BC005A', color: '#FFFFFF', border: '2px solid #BC005A' , textAlign: 'center'}}>
-                                        <b className="login2">Find Blood Donor</b>
-                                        </Button>
-                                    </div>
-                                </Col>
-                            </Row>
-                        </Container>
-                </div>
             </div>
             <Footer></Footer>
         </div>
