85b6766adca3040133495003dc356f20e634a4f5
Nufla Nasrudeen <87080421+Nufla-Nasrudeen@users.noreply.github.com>
2024-01-06 16:16:34 +0530

    Update index.js

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,1 +1,10 @@
+const dbConfig = require("../config/db.config.js");
 
+const mongoose = require("mongoose");
+
+const db = {};
+db.mongoose = mongoose;
+db.url = dbConfig.url;
+
+db.Donor = require("./donorModel.js")(mongoose);
+module.exports = db;
