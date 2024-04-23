37bd7136f191dd1cfb5eb7bbde50d9e496ea0069
nuflait <nufla2017@gmail.com>
2024-04-23 16:37:52 +0530

    import header and footer

--- frontend/blood_bank/src/pages/Finder/RequestBlood.jsx
+++ frontend/blood_bank/src/pages/Finder/RequestBlood.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Header from '../../components/common/Header';
+import Footer from '../../components/common/Footer';
 
+import { navLinks } from '../../assets/data/HeaderData';
+import { socialLinks, contactData } from '../../assets/data/FooterData';
 const RequestBlood = () => {
 
   return (
+    <div>
+      <div>
+        <Header navLinks={navLinks} />
+      </div>
+
+      <div>
+        <Footer navLinks1={socialLinks} navLinks2={contactData} />
+      </div>
+    </div>
     
   )
 }
