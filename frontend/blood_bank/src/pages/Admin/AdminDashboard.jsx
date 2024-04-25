538cbdb04eae9910ef0766ab39467e07d96d9746
Bhasinee Mawusika <87161642+Bhasinee@users.noreply.github.com>
2024-04-25 21:42:14 +0530

    Create AdminDashboard.jsx
    create AdminDashboard.jsx file

---
+++ frontend/blood_bank/src/pages/Admin/AdminDashboard.jsx
@@ -0,0 +1,91 @@
+import React, { useState, useEffect } from 'react'
+import { useLocation } from "react-router-dom";
+
+import { axiosGet } from '../../AxiosOperations';
+import Header from '../../components/common/Header';
+import Footer from '../../components/common/Footer';
+import { adminNavLinks } from '../../assets/data/HeaderData';
+import { socialLinks, contactData } from '../../assets/data/FooterData';
+import { Link } from 'react-router-dom';
+
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+
+
+const AdminDashboard = () => {
+
+
+  
+
+    const location = useLocation();
+    const searchParams = new URLSearchParams(location.search);
+    const id = searchParams.get("id");
+
+    useEffect(() => {
+
+        axiosGet(`donor/byid/${id}`)
+
+          
+            .catch(error => {
+
+                console.error('Error fetching data:', error);
+            });
+
+
+        return () => {
+            // Cleanup code goes here
+        };
+    }, []);
+
+
+    return (
+        <div >
+            <div>
+
+                <Header navLinks={adminNavLinks} donorloged={true} />
+            </div>
+            <div className='flex justify-center gap-4 p-[150px]'>
+                <div className='grid lg:grid-cols-4 md:grid-cols-2 gap-10 '>
+
+                    {
+                        adminNavLinks.map((item) =>
+                            <Stack direction="col" spacing={2}>
+                                {
+                                    <Button className='w-[250px] h-[50px]' variant="outlined" style={{ backgroundColor: '#BC005A', border: '2px solid white',color:'white' }} component={Link} to={item.path} >{item.display}</Button>
+                                }
+
+                            </Stack>
+                        )
+
+                    }
+
+
+
+
+
+                </div>
+            </div>
+
+            
+
+            {/* <div className='flex justify-center items-center text-[28px] font-bold w-full h-[500px] bg-slate-300'>
+
+        <div>
+          <span className='cardData text-[26px]' > {profile.fullname}</span>
+
+          <span className='cardData text-[26px]'>{profile.mobile}</span>
+          <span className='cardData text-[26px]'>{profile.district}  {profile.province}</span>
+          <span className='cardData text-[26px]'>{profile.lastdonationdate}</span>
+          <span className='cardData text-[26px]'>{profile.bloodgroup}</span>
+        </div>
+
+      </div>
+       */}
+            <div>
+                <Footer navLinks1={socialLinks} navLinks2={contactData} />
+            </div>
+        </div>
+    )
+}
+
+export default AdminDashboard
