f689db5ffff99e52d70e400cf209b854c0eaf343
aviruu <87100516+aviruu@users.noreply.github.com>
2024-04-29 13:22:02 +0530

    Update RegisterOtpSend.jsx

--- frontend/blood_bank/src/auth/RegisterOtpSend.jsx
+++ frontend/blood_bank/src/auth/RegisterOtpSend.jsx
@@ -23,7 +23,36 @@ const OtpSend = () => {
     const navigetor = useNavigate();
 
     return (
-       
+        <div className='flex justify-center items-center h-screen bg-stone-100 '>
+
+            <div className=' border-8 p-[75px]'>
+                <div>
+
+                    <center className='homepara text-[26px]'>Hi <span className='text-red-600 font-bold'>{decodedFormData.fullname}</span> </center>
+                    <center className='homepara text-[16px]'>Enter Your Email  {decodedFormData.email[0]}{decodedFormData.email[1]}**** {decodedFormData.email.substring(decodedFormData.email.length - 15)} </center>
+
+                </div>
+
+                <div className='w-full h-[300px] bg-slate-200 mt-[25px] p-[75px] border-2'>
+                    <center className='font-sans mb-[10px]'>Enter Email</center>
+                    <center className='font-sans mb-[10px] '><input type='text' className='w-[250px]' onChange={} ></input></center>
+
+                    <div>
+                        <center >
+                            <Button variant="contained" onClick={cheakOperations} endIcon={<SendIcon />}>
+                                Send
+                            </Button>
+                        </center>
+
+                    </div>
+
+
+                </div>
+
+            </div>
+
+
+        </div>
     )
 }
 
