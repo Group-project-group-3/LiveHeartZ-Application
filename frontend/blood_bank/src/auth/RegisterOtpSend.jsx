6495ddbbee7e37523533ee653972883c2c8d82cc
aviruu <87100516+aviruu@users.noreply.github.com>
2024-04-29 13:13:40 +0530

    Create RegisterOtpSend.jsx

---
+++ frontend/blood_bank/src/auth/RegisterOtpSend.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import { useLocation } from "react-router-dom";
+
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
+import { useNavigate } from "react-router-dom";
+import { axiosPost } from '../AxiosOperations';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+const OtpSend = () => {
+
+    return (
+       
+    )
+}
+
+export default OtpSend
