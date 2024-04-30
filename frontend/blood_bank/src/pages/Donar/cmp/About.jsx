99757c15c9b7a5ad31907a23c5ab6b366e78e566
Tekshan98mmadhawa <tekshanmadhawa324@gmail.com>
2024-04-30 07:13:20 +0530

    Updated About.jsx

--- frontend/blood_bank/src/pages/Donar/cmp/About.jsx
+++ frontend/blood_bank/src/pages/Donar/cmp/About.jsx
@@ -4,3 +4,9 @@ import Header from '../../components/common/Header';
 import Footer from '../../components/common/Footer';
 import {navLinks} from '../../assets/data/HeaderData';
 import {socialLinks,contactData} from '../../assets/data/FooterData';
+
+const About = () =>{
+    return(
+        
+    )
+}
