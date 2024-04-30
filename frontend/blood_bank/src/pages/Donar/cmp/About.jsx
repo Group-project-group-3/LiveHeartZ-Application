9ac62c06475f8b9893f10ff167b69ce263fde75a
Tekshan98mmadhawa <tekshanmadhawa324@gmail.com>
2024-04-30 07:05:48 +0530

    Updated Donar's about.jsx

---
+++ frontend/blood_bank/src/pages/Donar/cmp/About.jsx
@@ -0,0 +1,6 @@
+import React from 'React'
+
+import Header from '../../components/common/Header';
+import Footer from '../../components/common/Footer';
+import {navLinks} from '../../assets/data/HeaderData';
+import {socialLinks,contactData} from '../../assets/data/FooterData';
