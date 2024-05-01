c0741d6c3bb8271e459162955dc243d145292f85
Bhasinee Mawusika <87161642+Bhasinee@users.noreply.github.com>
2024-05-01 16:07:09 +0530

    Update Donor.jsx

--- frontend/blood_bank/src/pages/Admin/Donor.jsx
+++ frontend/blood_bank/src/pages/Admin/Donor.jsx
@@ -1,1 +1,6 @@
 import { Table } from 'antd'
+import React from 'react'
+import { useState, useEffect } from 'react';
+
+import { axiosGet } from '../../AxiosOperations';
+import Spinner1 from '../../pages/spinners/Spinner1';
