8574f9e3c826422c32df7a085aafc370f181ef7d
Bhasinee Mawusika <87161642+Bhasinee@users.noreply.github.com>
2024-05-01 16:13:40 +0530

    Update Donor.jsx

--- frontend/blood_bank/src/pages/Admin/Donor.jsx
+++ frontend/blood_bank/src/pages/Admin/Donor.jsx
@@ -6,7 +6,7 @@ import { axiosGet } from '../../AxiosOperations';
 import Spinner1 from '../../pages/spinners/Spinner1';
 
 const Donor = () => {
-    
+    const [donordata, setDonorData] = useState([]);
 }
 
 export default Donor
