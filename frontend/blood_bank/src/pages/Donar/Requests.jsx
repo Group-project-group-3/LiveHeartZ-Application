529f09dfb7f162f96b1429f2f9f40d6e8479b067
Vantae123 <87115907+Vantae123@users.noreply.github.com>
2024-05-02 21:31:02 +0530

    Create Requests.jsx

---
+++ frontend/blood_bank/src/pages/Donar/Requests.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react'
+import { useLocation } from "react-router-dom";
+
+import { axiosGet } from '../../AxiosOperations';
+import Header from '../../components/common/Header';
+import Footer from '../../components/common/Footer';
+import { donorNavLinks } from '../../assets/data/HeaderData';
+import { socialLinks, contactData } from '../../assets/data/FooterData';
+
+import { Table } from 'antd';
+import Report from '../Donar/popup/Report.jsx';
+
+const Requests = () => {
+
+  const [data, setData] = useState([]);
+
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const blood = searchParams.get("blood");
+
+
+  return (
+    <div >
+      <div>
+        <Header navLinks={donorNavLinks} donorloged={true} />
+      </div>
+
+      <div className="responsive-table flex justify-center pt-[40px] pb-[40px] bg-slate-100 h-[70vh]">
+        <Table className='border-2 p-[30px]' dataSource={[data][0]} columns={columns} />
+      </div>
+
+      <div>
+        <Footer navLinks1={socialLinks} navLinks2={contactData} />
+      </div>
+    </div>
+  )
+}
+
+export default Requests
