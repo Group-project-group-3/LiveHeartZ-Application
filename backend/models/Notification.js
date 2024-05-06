959c7d478b78d61d4098bf4df2268194a6a4d352
Bhasinee Mawusika <87161642+Bhasinee@users.noreply.github.com>
2024-05-06 14:46:09 +0530

    Update Notification.js

--- backend/models/Notification.js
+++ backend/models/Notification.js
@@ -1,7 +1,10 @@
 import mongoose from "mongoose";
 
 const NotificationSchema = new mongoose.Schema({
-
+    reciver_id: { type: String },
+    sendername: { type: String },
+    subject: { type: String },
+    message: { type: String },
 })
 
 export default mongoose.model('Notification', NotificationSchema);
