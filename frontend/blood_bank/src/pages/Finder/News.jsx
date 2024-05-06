fa00c8b680b09abc55c837b1fc5770caef50268f
FathimaAzma <140784165+FathimaAzma@users.noreply.github.com>
2024-05-06 19:43:38 +0530

    Importing the image for the news

--- frontend/blood_bank/src/pages/Finder/News.jsx
+++ frontend/blood_bank/src/pages/Finder/News.jsx
@@ -3,3 +3,4 @@ import Header from '../../components/common/Header';
 import { navLinks } from '../../assets/data/HeaderData';
 import { socialLinks, contactData } from '../../assets/data/FooterData';
 import Footer from '../../components/common/Footer';
+import bloodnews from '../../assets/images/News/bloodnews.webp';
