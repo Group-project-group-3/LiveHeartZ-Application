46f5204ec09074d5dcbabac594a17446b5a0ce3d
Bhasinee Mawusika <87161642+Bhasinee@users.noreply.github.com>
2024-05-06 14:48:56 +0530

    Update Reports.js

--- backend/models/Reports.js
+++ backend/models/Reports.js
@@ -1,3 +1,9 @@
 import mongoose from "mongoose";
-const ReportsSchema = new mongoose.Schema({})
+const ReportsSchema = new mongoose.Schema({
+    contactname: { type: String },
+    email: { type: String },
+    subject: { type: String },
+    message: { type: String },
+    name: { type: String }
+})
 export default mongoose.model('Reports', ReportsSchema);
