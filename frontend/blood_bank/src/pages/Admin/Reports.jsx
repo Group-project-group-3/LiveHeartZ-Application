4bba5ad55f3d8c5bb8de49d470d9388151df06f7
Bhasinee Mawusika <87161642+Bhasinee@users.noreply.github.com>
2024-05-06 14:13:43 +0530

    Update Reports.jsx

--- frontend/blood_bank/src/pages/Admin/Reports.jsx
+++ frontend/blood_bank/src/pages/Admin/Reports.jsx
@@ -1,6 +1,9 @@
 import { Button, Table } from 'antd'
 import React from 'react'
 import { useState, useEffect } from 'react';
+import { axiosGet } from '../../AxiosOperations';
+import Spinner1 from '../../pages/spinners/Spinner1';
+import { WarningOutlined } from '@ant-design/icons';
 
 const Report = () => {}
 
