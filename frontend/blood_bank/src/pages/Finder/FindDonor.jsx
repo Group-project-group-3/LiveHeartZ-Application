8e16ae2a4bc8e82558a0273582b22ffb1146cb29
Viethushaa <146052855+Viethushaa@users.noreply.github.com>
2024-05-06 20:48:28 +0530

    Update FindDonor.jsx

--- frontend/blood_bank/src/pages/Finder/FindDonor.jsx
+++ frontend/blood_bank/src/pages/Finder/FindDonor.jsx
@@ -193,19 +193,12 @@ const FindDonor = () => {
             </div>
 
             <div>
-              <label className="block text-sm font-medium text-gray-700 mb-[5px]">
-                District
-              </label>
-              <select name="district" onChange={handleChnage} className='w-full h-[40px] bg-slate-100'>
-                <option value="Select">Select</option>
-                {
 
-                  districts.map(dis =>
-                    <option value={dis}>{dis}</option>
-                  )
 
-                }
-              </select>
+
+
+
+              
               {errors.district && <span className="text-red-500">{errors.district}</span>}
             </div>
             <div className='flex'>
@@ -213,6 +206,9 @@ const FindDonor = () => {
                 
                 <Button variant="outlined" style={{ color: '#BC005A', border: '2px solid #BC005A' }} onClick={getDonorsList} >FIND DONARS</Button>
               </Stack>
+
+
+              
             </div>
           </div>
         </form>
