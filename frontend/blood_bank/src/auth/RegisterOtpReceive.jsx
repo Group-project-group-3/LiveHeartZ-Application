144e6b2cad27bb24ce8cdf6e89bedf45925e6fbc
aviruu <87100516+aviruu@users.noreply.github.com>
2024-05-06 13:10:03 +0530

    Update RegisterOtpReceive.jsx

--- frontend/blood_bank/src/auth/RegisterOtpReceive.jsx
+++ frontend/blood_bank/src/auth/RegisterOtpReceive.jsx
@@ -6,8 +6,23 @@ import SendIcon from '@mui/icons-material/Send';
 import { axiosPost } from '../AxiosOperations';
 import { useNavigate } from 'react-router-dom';
 
+const OtpReceive = () => {
+
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const formdata = searchParams.get("formdata");
+
+  const [otp, setOtp] = useState(null);
+  var decodedFormData = JSON.parse(formdata);
+  
+  }
   return (
-    
+    <div className='flex justify-center items-center h-screen bg-stone-100 '>
+
+      <div className=' border-8 p-[75px]' >
+
+      </div>
+    </div>
   )
 
 
