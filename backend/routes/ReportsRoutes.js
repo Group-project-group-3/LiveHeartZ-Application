3ec1fc3ecc9233540caad92d6db147c1387a95d5
SafnaZarook <87115907+SafnaZarook@users.noreply.github.com>
2024-05-06 22:04:20 +0530

    Create ReportsRoutes.js

---
+++ backend/routes/ReportsRoutes.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { setReports, getReportCount } from '../controllers/ReportsController.js';
+
+const router = express.Router();
+
+router.post('/setreport', setReports);
+router.get('/getreport', getReportCount);
+
+export default router;
