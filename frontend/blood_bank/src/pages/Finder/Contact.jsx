3bd4fa6eb31eec3a81166b3b74d241152d612552
Tekshan98mmadhawa <tekshanmadhawa324@gmail.com>
2024-05-06 22:29:22 +0530

    Update Contact Page

---
+++ frontend/blood_bank/src/pages/Finder/Contact.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import Header from '../../components/common/Header';
+import { navLinks } from '../../assets/data/HeaderData';
+import { socialLinks,contactData } from '../../assets/data/FooterData';
+import Footer from '../../components/common/Footer';
+
+import { FaPhone,FaLocationDot } from "react-icons/fa6";
+import { CgWebsite } from "react-icons/cg";
+import { MdEmail } from "react-icons/md";
