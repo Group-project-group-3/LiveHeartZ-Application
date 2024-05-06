b83cf4cf7898d0487198b193b50b8b7882b21c11
FathimaAzma <140784165+FathimaAzma@users.noreply.github.com>
2024-05-06 21:11:33 +0530

    SearchDonor component definition and Define state variables

--- frontend/blood_bank/src/pages/Finder/SearchDonor.jsx
+++ frontend/blood_bank/src/pages/Finder/SearchDonor.jsx
@@ -4,3 +4,14 @@ import { axiosGet, axiosPost } from '../../AxiosOperations';
 import { useState,useEffect } from 'react';
 import Spinner1 from '../../pages/spinners/Spinner1';
 import testImg from '/user.png';
+
+const SearchDonor = () => {
+  const [donorList, setDonorList] = useState([]); 
+  const [loading, setLoading] = useState(true);
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const requesterData = searchParams.get("data");
+  var decodedFormData = JSON.parse(requesterData);
+}
+
+export default SearchDonor
