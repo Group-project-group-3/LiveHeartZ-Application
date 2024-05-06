1032d6109ce5e4fc9e31c897c630a5b1a2c16e57
aviruu <87100516+aviruu@users.noreply.github.com>
2024-05-06 13:07:50 +0530

    Create RegisterOtpReceive.jsx

---
+++ frontend/blood_bank/src/auth/RegisterOtpReceive.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import { useLocation } from "react-router-dom";
+
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
+import { axiosPost } from '../AxiosOperations';
+import { useNavigate } from 'react-router-dom';
+
+  return (
+    
+  )
+
+
+export default OtpReceive
