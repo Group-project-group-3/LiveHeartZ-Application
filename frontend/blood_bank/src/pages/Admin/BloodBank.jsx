603ece4fcb4ba9515c89aa3f0b91afea41c48815
ChathuryaHiruni <149374708+ChathuryaHiruni@users.noreply.github.com>
2024-05-06 10:37:38 +0530

    Update BloodBank.jsx

--- frontend/blood_bank/src/pages/Admin/BloodBank.jsx
+++ frontend/blood_bank/src/pages/Admin/BloodBank.jsx
@@ -1,3 +1,35 @@
 import { Table } from 'antd'
 import React from 'react'
 import { useState, useEffect } from 'react';
+import { axiosGet } from '../../AxiosOperations';
+const BloodBank = () => {
+
+  const [blooddata, setBloodData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+
+  const addCount = async (id, count) => {
+
+    try {
+
+      await axiosGet(`home/bloodbank/updatecount/${id}/${count}/add`).then(alert("Succesfully Added Blood Unit"))
+
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
+  const subCount = async (id, count) => {
+
+
+    try {
+
+      await axiosGet(`home/bloodbank/updatecount/${id}/${count}/sub`).then(alert("Succesfully Delete Blood Unit"));
+
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
+
+}
