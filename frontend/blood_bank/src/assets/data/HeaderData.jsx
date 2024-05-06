c4ad0121548967713663431ab15f15297fbb6f84
FathimaAzma <140784165+FathimaAzma@users.noreply.github.com>
2024-05-06 18:40:46 +0530

    Define navigation links for regular users

---
+++ frontend/blood_bank/src/assets/data/HeaderData.jsx
@@ -0,0 +1,18 @@
+const navLinks = [
+    {
+        path: '/',
+        display: 'HOME'
+    },
+    {
+        path: '/about',
+        display: 'ABOUT US'
+    },
+    {
+        path: '/news',
+        display: 'NEWS'
+    },
+    {
+        path: '/contact',
+        display: 'CONTACT US'
+    },
+]
