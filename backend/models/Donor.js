5348773f8ca50a2c37d8346ba2653e59aa69dcb4
aviruu <87100516+aviruu@users.noreply.github.com>
2024-05-08 10:53:23 +0530

    Update Donor.js

--- backend/models/Donor.js
+++ backend/models/Donor.js
@@ -7,7 +7,15 @@ const DonorSchema = new mongoose.Schema({
     mobile: { type: Number },
     password: { type: String },
     bloodgroup: { type: String },
-    gender: { type: String }
+    gender: { type: String },
+    birthdate: { type: String },
+    weight: { type: Number },
+    lastdonationdate: { type: String },
+    zipcode: { type: String },
+    province: { type: String },
+    tandc: { type: Boolean },
+    district: { type: String },
+    verified: { type: Boolean }
 }
 
 )
