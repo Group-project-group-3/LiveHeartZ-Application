3c30069e9e90189c55c33e358e17f493eea13dde
SafnaZarook <87115907+SafnaZarook@users.noreply.github.com>
2024-05-15 12:16:41 +0530

    Update Profile.jsx

--- frontend/blood_bank/src/pages/Donar/Profile.jsx
+++ frontend/blood_bank/src/pages/Donar/Profile.jsx
@@ -14,14 +14,14 @@ import Stack from '@mui/material/Stack';
 
 const Profile = () => {
 
-  
+
   const [profile, setProfile] = useState([]);
 
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
   const id = searchParams.get("id");
 
-    useEffect(() => {
+  useEffect(() => {
 
     axiosGet(`donor/byid/${id}`)
 
@@ -35,47 +35,46 @@ const Profile = () => {
         console.error('Error fetching data:', error);
       });
 
+
     return () => {
       // Cleanup code goes here
     };
   }, []);
 
-  
+
+
   return (
     <div >
       <div>
         {<Header navLinks={donorNavLinks} donorloged={true} />}
       </div>
-      <div className='flex justify-center gap-4 p-[20px] bg-gray-800'>
+      <div className='flex justify-center gap-4 p-[20px]'>
         <div className='grid lg:grid-cols-2 md:grid-cols-2 gap-10 '>
-
           {
             donorNavLinks.map((item) =>
               <Stack direction="col" spacing={2}>
                 {
-                  item.display === "REQUESTS" ? (<Button className='w-[250px] h-[50px]' variant="outlined" component={Link} to={`/donor/requests?blood=${encodeURIComponent(profile.bloodgroup)}`} >{item.display}</Button>)
-                    : (<Button className='w-[250px] h-[50px]' variant="outlined" component={Link} to={`/donor/notifications?id=${encodeURIComponent(profile._id)}`} >{item.display}</Button>)
+                  item.display === "REQUESTS" ? (<Button className='w-[250px] h-[50px]' variant="outlined" style={{ backgroundColor: '#BC005A', border: '2px solid white', color: 'white' }} component={Link} to={`/donor/requests?blood=${encodeURIComponent(profile.bloodgroup)}`} >{item.display}</Button>)
+                    : (<Button className='w-[250px] h-[50px]' variant="outlined" style={{ backgroundColor: '#BC005A', border: '2px solid white', color: 'white' }} component={Link} to={`/donor/notifications?id=${encodeURIComponent(profile._id)}`} >{item.display}</Button>)
                 }
-
               </Stack>
             )
 
-
-
-            
           }
         </div>
       </div>
 
-      <div className='flex justify-center items-center text-[28px] font-bold w-full h-[500px] bg-slate-300'>
+      <div className='flex justify-center items-center text-[28px] font-bold w-full h-[400px] '>
 
         <div>
-          <span className='cardData text-[26px]' > {profile.fullname}</span>
+          <span className='cardData text-[26px]' >Name : {profile.fullname}</span>
 
-          <span className='cardData text-[26px]'>{profile.mobile}</span>
-          <span className='cardData text-[26px]'>{profile.district}  {profile.province}</span>
-          <span className='cardData text-[26px]'>{profile.lastdonationdate}</span>
-          <span className='cardData text-[26px]'>{profile.bloodgroup}</span>
+          <span className='cardData text-[26px]'>Mobile Number : {profile.mobile}</span>
+          <span className='cardData text-[26px]'>District : {profile.district}  {profile.province}</span>
+          <span className='cardData text-[26px]'>Province : {profile.province}</span>
+          <span className='cardData text-[26px]'>Blood Group : {profile.bloodgroup}</span>
+          <span className='cardData text-[26px]'>Last Donation Date : {profile.lastdonationdate}</span>
+
         </div>
 
       </div>
