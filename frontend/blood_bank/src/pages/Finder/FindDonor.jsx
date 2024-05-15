9cae8a7d48c4c155ebd3b886fa85ed13adaa4c77
Viethushaa <146052855+Viethushaa@users.noreply.github.com>
2024-05-15 21:37:46 +0530

    Last Update FindDonor.jsx

--- frontend/blood_bank/src/pages/Finder/FindDonor.jsx
+++ frontend/blood_bank/src/pages/Finder/FindDonor.jsx
@@ -1,4 +1,3 @@
-//import the packages for the use
 import React, { useState, useEffect } from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
@@ -20,30 +19,28 @@ import Province from '../../assets/data/SelectData';
 
 const FindDonor = () => {
 
-  const [verifyuser, setVerifyUser] = useState(false);//Here, it initializes verifyuser state variable with false value and provides a function setVerifyUser to update its value.
-  const location = useLocation();//useLocation() is a custom hook likely from a library such as React Router) used to access the current URL location in a React component.
-  const searchParams = new URLSearchParams(location.search);//new URLSearchParams(location.search) creates a new URLSearchParams object from the query parameters in the current URL.
-  const verify = searchParams.get("verify");//It  retrieves the value of the query parameter named "verify" from the URL, if it exists.
-  const requesterData = searchParams.get("data");//It retrieves the value of the query parameter named "data" from the URL, if it exists.
-  var decodedFormData = JSON.parse(requesterData);//It attempts to parse the value of the "data" query parameter as JSON. requesterData is the value retrieved from the URL.
+  const [verifyuser, setVerifyUser] = useState(false);
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const verify = searchParams.get("verify");
+  const requesterData = searchParams.get("data");
+  var decodedFormData = JSON.parse(requesterData);
 
 
-//This part of the code is utilizing the useEffect hook
-  //and if the URL parameter "verify" is set to "true", it updates the verifyuser state variable to true
-useEffect(() => {
+
+  useEffect(() => {
+
     if (verify == "true") {
       setVerifyUser(true);
     }
 
 
-  //It is an optional cleanup function that runs when the component unmounts or before the effect runs again. 
-  return () => {
+    return () => {
       // Cleanup code goes here
     };
   }, []);
 
 
-  //This code sets up various pieces of information that a React component might need:
   const [districts, setDistricts] = useState([]);
   const [errors, setErrors] = useState({});
   const [loading, setLoading] = useState(true);
@@ -51,15 +48,14 @@ useEffect(() => {
   const [chatVisible, setChatVisible] = useState(false);
   const [available, setAvailable] = useState(false);
 
-  //This state variable can be used to manage form data in a React component, 
-  //typically used for capturing user input such as selecting a blood group, province, and district
+
+
   const [formData, setFormData] = useState({
     bloodgroup: 'Select',
     province: 'Select',
     district: 'Select',
   });
 
-  // making an asynchronous HTTP request using Axios to check the availability of blood in a blood bank based on the selected blood group 
   const bloodbankCheck = () => {
     try {
       axiosGet(`home/bloodbank/availability/${formData.bloodgroup}`).then((data) => {
@@ -75,17 +71,15 @@ useEffect(() => {
     } catch (error) {
 
     }
+
   }
 
-  //function is used to handle changes in form input fields. 
-  //It takes an event object e as a parameter, typically triggered by a user's interaction with an input field.
   const handleChnage = (e) => {
     const { value, name } = e.target;
     setFormData({ ...formData, [name]: value });
 
   };
 
-  //This getDistricts function seems to be designed to get the districts corresponding to the selected province.
   const getDistrcts = (e) => {
 
     Province.map(pr => {
@@ -97,7 +91,6 @@ useEffect(() => {
 
   };
 
-  //This sendMail function is an asynchronous function that sends an email to a recipient identified by id
   const sendMail = async (id) => {
 
     try {
@@ -111,8 +104,6 @@ useEffect(() => {
   };
 
 
-  // It checks if the selected options for blood group, province, and district are valid (not equal to "Select").
-  //If any of these fields are not selected, it sets an appropriate error message in the errors object.
   const validateFormData = () => {
     const errors = {};
     if (formData.bloodgroup.trim() === "Select") {
@@ -128,7 +119,6 @@ useEffect(() => {
     return Object.keys(errors).length === 0;
   };
 
-  //This function seems to be responsible for fetching a list of donors based on the selected blood group, province, and district.
   const getDonorsList = async () => {
     if (validateFormData()) {
       await axiosGet(`donor/finddonor/${formData.bloodgroup}/${formData.province}/${formData.district}`)
@@ -145,13 +135,13 @@ useEffect(() => {
     }
   };
 
-  //It likely intends to handle some functionality related to opening a report box,
   const openReportBox = () => {
 
   }
 
-  return(
-     <div>
+
+  return (
+    <div>
       <Header navLinks={navLinks} />
       <div className='flex flex-col'>
         <form>
@@ -174,7 +164,6 @@ useEffect(() => {
               </select>
               {errors.bloodgroup && <span className="text-red-500">{errors.bloodgroup}</span>}
             </div>
-
             <div>
               <label className="block text-sm font-medium text-gray-700 mb-[5px]">
                 Province
@@ -191,7 +180,8 @@ useEffect(() => {
               </select>
               {errors.province && <span className="text-red-500">{errors.province}</span>}
             </div>
-             <div>
+
+            <div>
               <label className="block text-sm font-medium text-gray-700 mb-[5px]">
                 District
               </label>
@@ -209,7 +199,7 @@ useEffect(() => {
             </div>
             <div className='flex'>
               <Stack direction="row" spacing={2}>
-                
+
                 <Button variant="outlined" style={{ color: '#BC005A', border: '2px solid #BC005A' }} onClick={getDonorsList} >FIND DONARS</Button>
               </Stack>
             </div>
@@ -217,42 +207,64 @@ useEffect(() => {
         </form>
       </div>
 
-       {
-        donorList.length && available && <center className='text-[26px] homepara text-red-400 underline'>Your blood s avilable at our blood bank you can collect or you can contact thease donors.</center>
+
+      {
+        donorList.length && available && (
+          <center className='text-[26px] homepara text-red-400 underline'>
+            The blood is available at our blood bank. You can pick it up.
+            <a href="https://www.google.com/maps/place/University+of+Vavuniya/@8.7559478,80.4106635,19.14z/data=!4m7!3m6!1s0x3afc3f3a01dd2393:0xe31eaf7e0d5bcecd!4b1!8m2!3d8.7586746!4d80.4106966!16s%2Fg%2F11my146w8q?entry=ttu" target="_blank" rel="noopener noreferrer">
+              <span style={{ color: 'blue', textDecoration: 'none' }}> Click here for location. </span>
+            </a>
+            <br></br>
+            or contact the donors below.
+          </center>
+        )
       }
 
-        {donorList.length !== 0 ? (
+
+
+      {donorList.length !== 0 ? (
         loading ? (<Spinner1 />) : (<div className='grid xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 gap-4'>
           {donorList.map(item => (
             <div className='flex justify-center p-[20px]' key={item._id}>
               <div className='xl:w-[500px] lg:w-[500px] md:w-[500px] h-[300px] border-2 bg-slate-200  '>
                 <div className='flex justify-around  '>
                   <div className='w-[200px] h-[200px]  mt-[20px]  '><span><img src={testImg} alt="" /></span></div>
-                  <div className='w-[200px] h-[200px] mt-[40px]  ' >
-                    <div>
-                      <span key='1' className='cardData text-[24px] font-bold'>{item.fullname}</span>
-                      <i className="fa-solid fa-phone"></i><span key='2' className='cardData'>{item.mobile}</span>
-                      <i className="fa-solid fa-location-dot"></i><span key='3' className='cardData'>{item.district} , {item.province}.</span>
-                      <i className="fa-regular fa-calendar"></i><span key='5' className='cardData'>Last Donation : {item.lastdonationdate}</span>
+                  <div className='w-[200px] h-[200px] mt-[40px]'>
+                    <div className='flex flex-col'>
+                      <span key='1' className='cardData text-[24px] font-bold mb-3'>{item.fullname}</span>
+                      <div className='flex flex-col'>
+                        <div className='flex items-center mb-3'>
+                          <i className="fa-solid fas fa-tint"> :</i>
+                          <span key='2' className='cardData ml-2'>Blood Group: {item.bloodgroup}</span>
+                        </div>
+                        <div className='flex items-center mb-3'>
+                          <i className="fa-solid fa-location-dot"></i>
+                          <span key='3' className='cardData ml-2'>Address: {item.district}, {item.province}.</span>
+                        </div>
+                        <div className='flex items-center mb-3'>
+                          <i className="fa-regular fa-calendar"></i>
+                          <span key='5' className='cardData ml-2'>Last Donation: {item.lastdonationdate}</span>
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
 
-                
                 {
                   verifyuser ? (<div className='grid grid-cols-2'>
                     <div>
                       {chatVisible && <Chat name={item.fullname} id={item._id} />}
                     </div>
                     <div>
-                      {chatVisible && <Button style={{ backgroundColor: '#BC005A', border: '2px solid white',color: 'white' }} onClick={() => sendMail(item._id)} className='w-full'>
+                      {chatVisible && <Button style={{ backgroundColor: '#BC005A', border: '2px solid white', color: 'white' }} onClick={() => sendMail(item._id)} className='w-full'>
                         Request Email
                       </Button>}
 
                     </div>
                   </div>) : (
                     <div className='flex justify-between '>
-                      <Link className='text-[16px] homepara text-green-800 font-bold w-full flex justify-center ' style={{ color: '#BC005A', border: '2px solid #BC005A' }} to={'/requestblood'}>SEND REQUEST</Link>
+                      <Link className='text-[16px] homepara text-green-800 font-bold w-full flex justify-center ' style={{ borderRadius: 4, alignItems: 'center', color: '#BC005A', border: '2px solid #BC005A' }} to={'/requestblood'}>SEND REQUEST</Link>
                       <Report name={item.fullname} email={item.email} />
 
                     </div>)
@@ -263,14 +275,13 @@ useEffect(() => {
           ))}
         </div>)
 
-        ) : (
-        <div className='flex justify-center w-full/2 h-[500px] items-center bg-slate-200 m-[20px] homepara'>No Available Donor/Not Select All Fields</div>
+      ) : (
+        <div className='flex justify-center w-full/2 h-[500px] items-center bg-slate-200 m-[20px] homepara'>There is no available donor / All fields are not selected</div>
       )}
 
       <Footer navLinks1={socialLinks} navLinks2={contactData} />
-    </div>  
-  );           
+    </div>
+  );
 }
+
 export default FindDonor;
-
-
